3e89be1a50761865b3b6c797a8047af7f697b892
shan230104 <tsaishanmukh@gmail.com>
2025-07-29 01:37:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'process.env.FRONTEND_URL',
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     })
 )
