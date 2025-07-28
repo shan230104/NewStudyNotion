ca4f3bd5c13031db043eafb0926197779f5527ac
shan230104 <tsaishanmukh@gmail.com>
2025-07-29 00:20:15 +0530

    Update razorpay.js

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,6 +1,13 @@
 const Razorpay = require('razorpay');
 
-exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY,
-    Key_secret: process.env.RAZORPAY_SECRET,
-}); 
+
+if (process.env.RAZORPAY_KEY_ID && process.env.RAZORPAY_KEY_SECRET) {
+    exports.instance = new Razorpay({
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+} else {
+    exports.instance = null;
+    console.warn("WARNING: Razorpay keys are not provided. Razorpay integration will be disabled.");
+   
+}
